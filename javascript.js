e723a93df57714167cfd3d91faaa07b8bb944698
Abantika Saha <abantika.saha18@gmail.com>
2021-02-28 17:52:47 +0530

    cond js

---
+++ javascript.js
@@ -0,0 +1,12 @@
+let dayOfWeek = prompt('ENTER A DAY').toLowerCase();
+// console.log('MEH');
+
+if (dayOfWeek === 'monday') {
+    console.log("UGHHH I HATE MONDAYS!");
+} else if (dayOfWeek === 'saturday') {
+    console.log("YAY I LOVE SATURDAYS!");
+} else if (dayOfWeek === 'friday') {
+    console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!");
+} else {
+    console.log("MEH");
+}
