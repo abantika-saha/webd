91d9e82a08f599e92b3b08b91293a75ba6ca724b
Abantika Saha <abantika.saha18@gmail.com>
2021-04-08 08:56:34 +0530

    passing data to templates

--- kahagaye.js
+++ kahagaye.js
@@ -6,6 +6,11 @@ app.set('view engine', 'ejs');
 app.get('/',(req,res)=>{
     res.render('home.ejs')
 })
+
+app.get('/rand',(req,res)=>{
+    const num = Math.floor(Math.random()*10)+1
+    res.render('random',{rand:num})
+})
 app.listen(3000, () => {
     console.log("Listening on 3000 port")
 })
