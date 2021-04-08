7446e27b384aa77bad9a048cfa7996a91962414c
Abantika Saha <abantika.saha18@gmail.com>
2021-04-08 09:02:28 +0530

    subreddit template demo

--- kahagaye.js
+++ kahagaye.js
@@ -7,6 +7,13 @@ app.get('/',(req,res)=>{
     res.render('home.ejs')
 })
 
+app.get('/r/:subreddit',(req,res)=>{
+    const {subreddit} = req.params;
+    res.render('subreddit',{subreddit})
+
+})
+
+
 app.get('/rand',(req,res)=>{
     const num = Math.floor(Math.random()*10)+1
     res.render('random',{rand:num})
